dd9e3652a69730c851aad4001d7f85f9544536dc
Anwell Wang <anwellwang@gmail.com>
2014-01-01 19:44:37 -0800

    added dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject fenetk "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "http://fene.tk/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [ring "1.0.1"]
+                 [compojure "1.0.1"]
+                 [enlive "1.0.0"]])

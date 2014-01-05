fcc7ace4877b4acabb618746c6e7458411dd027c
Anwell Wang <anwellwang@gmail.com>
2014-01-04 19:02:25 -0800

    map urls

---
+++ bin/fenetk/core_test.clj
@@ -0,0 +1,7 @@
+(ns fenetk.core-test
+  (:require [clojure.test :refer :all]
+            [fenetk.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
---
+++ src/fenetk/app.clj
@@ -0,0 +1,29 @@
+(ns fenetk.app
+  (:use (compojure handler
+                   [core :only (GET POST defroutes)]))
+  (:require [net.cgrand.enlive-html :as en]
+            [ring.util.response :as response]
+            [ring.adapter.jetty :as jetty]))
+
+(defonce counter (atom 10000))
+
+(defonce urls (atom {}))
+
+(defn shorten
+  [url]
+  (let [id (swap! counter inc)
+        id (Long/toString id 36)]
+    (swap! urls assoc id url)
+    id))
+
+(defn homepage
+  [request]
+  (str @urls))
+
+(defn redirect
+  [id]
+  (response/redirect (@urls id)))
+
+(defroutes app
+  (GET "/" request (homepage request))
+  (GET "/:id" [id] (redirect id)))
